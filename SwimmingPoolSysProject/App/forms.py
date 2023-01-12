3746a8c3d6ab8c495ff98cc29a418758eaba65a8
MadPapa <happyekkov@gmail.com>
2023-01-12 17:29:49 +0100

    changing forms email type field, adding css style to it

--- SwimmingPoolSysProject/App/forms.py
+++ SwimmingPoolSysProject/App/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=255)
-    email = forms.CharField(max_length=255)
+    email = forms.EmailField(widget=forms.EmailInput)
     message = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols':25}))
