ad4776f17565fbd144341844c0a709b6c032a895
MadPapa <happyekkov@gmail.com>
2023-01-12 12:23:04 +0100

    connecting frontend to App, including static files, adding static dir path in project settings

--- SwimmingPoolSysProject/App/urls.py
+++ SwimmingPoolSysProject/App/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homePage, name='home')
+    path('', views.homePage, name='home'),
+    path('login/', views.login, name='login'),
+    path('registration/', views.registration, name='registration'),
+    path('remind_password/', views.remind_password, name='remind_password'),
 ]
--- SwimmingPoolSysProject/App/views.py
+++ SwimmingPoolSysProject/App/views.py
@@ -3,5 +3,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def homePage(request):
-    return HttpResponse('<h1>Tu bedzie kozak apka xD</h1>')
+    return render(request, 'App/index.html')
+
+
+def login(request):
+    return render(request, 'App/subpages/login.html')
+
+
+def registration(request):
+    return render(request, 'App/subpages/registration.html')
+
+
+def remind_password(request):
+    return render(request, 'App/subpages/remind_password.html')
