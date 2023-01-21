b0c6093ae7e003ee20835108e6d83d6c017e0ed1
Vex0on <Iplayagame101@gmail.com>
2023-01-21 20:45:02 +0100

    Small css fix

--- SwimmingPoolSysProject/App/forms.py
+++ SwimmingPoolSysProject/App/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=255)
     email = forms.EmailField(widget=forms.EmailInput)
-    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 2, 'cols':25}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'rows': 5, 'cols': 25}))
